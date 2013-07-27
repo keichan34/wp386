891a8ac29338a9dea15d5d8e32f4172b8d32ded1
Keitaroh Kobayashi <keita@kkob.us>
2013-07-27 12:25:19 -0400

    SCSS via compass and the bootstrap theme

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site container">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
