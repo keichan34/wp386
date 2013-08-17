7cc51ec731150d92f8e062bd62d65f154082d7ff
Keitaroh Kobayashi <keita@kkob.us>
2013-08-17 12:28:17 +0900

    puts the footer inside of the main column

--- footer.php
+++ footer.php
@@ -8,18 +8,18 @@
  */
 ?>
 
+				<footer id="colophon" class="site-footer" role="contentinfo">
+					<div class="site-info container">
+						<?php do_action( 'wp386_credits' ); ?>
+						<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wp386' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wp386' ), 'WordPress' ); ?></a>
+						<span class="sep"> | </span>
+						<?php printf( __( 'Theme: %1$s by %2$s.', 'wp386' ), 'wp386', '<a href="http://themes.kkob.us/wp386" rel="designer">Keitaroh Kobayashi</a>' ); ?>
+					</div><!-- .site-info -->
+				</footer><!-- #colophon -->
+
       </div><!-- .span9 -->
     </div><!-- .row -->
 	</div><!-- #main -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info container">
-			<?php do_action( 'wp386_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wp386' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wp386' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'wp386' ), 'wp386', '<a href="http://themes.kkob.us/wp386" rel="designer">Keitaroh Kobayashi</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
